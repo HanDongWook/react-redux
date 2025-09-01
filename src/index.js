ff5a6edef2cf48562d6269561e7f9e116e9156cf
HanDongWook <jja9312345@gmail.com>
2025-09-01 21:18:00 +0900

    Connecting the Store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "../store";
 
-// 1. 'document.getElementById("root")'를 변수(root)에 할당합니다.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// 2. 할당된 'root' 변수의 render() 메서드를 호출합니다.
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
