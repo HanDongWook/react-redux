3f3720e41c88605cfe921b7feeb8ce5e56ae7562
HanDongWook <jja9312345@gmail.com>
2025-09-02 09:15:45 +0900

    createSlice

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 
+
 function Detail({ toDos }) {
     const myId = useParams().id;
     const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
