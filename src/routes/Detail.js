6cd58ceb00416d614025b2cab3dd34bfa4670ece
HanDongWook <jja9312345@gmail.com>
2025-09-02 09:18:35 +0900

    update Detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,10 +6,11 @@ import { useParams } from "react-router-dom";
 function Detail({ toDos }) {
     const myId = useParams().id;
     const toDo = toDos.find((toDo) => toDo.id === parseInt(myId));
+    const dateObject = new Date(toDo?.id);
     return (
         <>
-            <h1>{toDo?.text}</h1>
-            <h5>Created at: {toDo?.id}</h5>
+            <h1>ToDo : {toDo?.text}</h1>
+            <h5>Created at: {dateObject.toLocaleString()}</h5>
         </>
     );
 }
